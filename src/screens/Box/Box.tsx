9889cb8c6cb383fa4c6c0a88b2071d09b27257cb
Andreas Wallner <andreaswallner@MacBook-Pro-von-Wallner.local>
2025-10-03 19:43:31 +0200

    Update'

--- src/screens/Box/Box.tsx
+++ src/screens/Box/Box.tsx
@@ -322,7 +322,7 @@ export const Box = (): JSX.Element => {
               LITTMANN<span className="text-[#BFD9DC]"> INFOS</span>
             </h2>
             <p className="text-gray-300 text-sm sm:text-base leading-relaxed mb-6 sm:mb-8 text-center">
-              © Copyright | LITTMANN-ENTERTAINMENT | All Rights Reserved
+              © Copyright | LITTMANN ENTERTAINMENT | All Rights Reserved
             </p>
 
             <div className="flex justify-center gap-4 mb-6">
